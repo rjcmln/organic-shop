4aaade417c604fbae37e52b6ec37411fb5a8b0f5
Milan Rajic <rjcmln@gmail.com>
2019-03-19 01:02:45 +0100

    Search products, clients side. Observables in action (multiple windows)

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,17 +1,33 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent {
+export class AdminProductsComponent implements OnDestroy {
 
-  products$;
+  products: any[];
+  fiteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = productService.getAll();
+    this.subscription = productService.getAll().subscribe(products => {
+      console.log("productService.getAll");
+      this.fiteredProducts = this.products = products;
+    });
   }
 
+  filter(query: string) {
+    this.fiteredProducts = (query) ?
+      this.products.filter(p => p.payload.child('title').val().toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+    this.subscription.unsubscribe();
+  }
 }
